2453b67308f499f3a8a4c70637539a78c0a79ce5
hyoming <hyoming9749@gmail.com>
2023-03-30 00:13:14 +0900

    웹페이지 구현중

--- web.js
+++ web.js
@@ -8,7 +8,17 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index2');
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// 게시판 라우팅 추가하기
+app.get('/board/:name', (req, res) => {
+    const boardName = req.params.name;
+    res.render(`board/${boardName}`, { nameTest: boardName });
 });
 
 app.listen(8080, () => {
